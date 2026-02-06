0a72da787b9e84342907d3bb9008e2baaacb6f32
adam hazim <adamhazim030@gmail.com>
2026-02-06 11:38:36 +0800

    rekod kehadiran

--- script.py
+++ script.py
@@ -1,6 +1,24 @@
 import os
 
-if os.path.exists("index.html"):
-    print("Fail index.html wujud.")
-else:
-    print("Fail index.html tidak dijumpai.")
+def test():
+    file_name = "index.html"
+    print(f"Memulakan semakan pada: {file_name}")
+
+    # Menyemak jika fail index.html wujud dalam folder
+    if os.path.exists(file_name):
+        print("PENGESAHAN: Fail index.html ditemui.")
+    else:
+        print("RALAT: Fail index.html tidak dijumpai!")
+        exit(1)
+
+    # Menyemak jika fungsi alert() telah ditulis dalam kod
+    with open(file_name, "r") as f:
+        kandungan = f.read()
+        if "alert(" in kandungan:
+            print("PENGESAHAN: Fungsi alert dikesan dalam kod.")
+        else:
+            print("RALAT: Fungsi alert tidak dijumpai!")
+            exit(1)
+
+if __name__ == "__main__":
+    test()
